1bc48bf84dd94f628f1fab222bbd17fb432bd02c
Qaisar Ali Mian <qaisaralimian65@gmail.com>
2026-09-14 19:54:11 +0500

    OOPs folfer

---
+++ OOPs/OOPS1.dart
@@ -0,0 +1,36 @@
+//instanve vs class variable
+void main() {
+  var a = Vehical();
+  var b = Vehical();
+  a.x = 1;
+  a.y = 2;
+
+  b.x = 3;
+  b.y = 4;
+  // a have x,y its own;
+  // b have x,y its own
+
+  print(a.x);
+  print(b.x);
+
+  print(a.y);
+  print(b.y);
+
+  print("COunter");
+  print(Vehical.counter);
+}
+
+//class variable belongs to class not to object
+class Vehical {
+  //instance variable
+  int? x;
+  int? y;
+  // constructor
+  Vehical() {
+    counter++;
+  }
+
+  // class variable(Keyword staitc);
+
+  static int counter = 0;
+}
